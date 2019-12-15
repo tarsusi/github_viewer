5e03abf3d346b6be8ebcd47b943fc0c2dac6c15a
Ömer Burak KARATAŞ <omer.burak.karatas@outlook.com>
2019-12-15 19:38:01 +0300

    refactor(repository-service): add getRepositoryDetails API service

--- app/services/repositoryService.js
+++ app/services/repositoryService.js
@@ -7,13 +7,35 @@ export const getUserRepositories = (userName) => {
       message,
       repositories:
         repositories &&
-        repositories.map(({ name, id, description, html_url: detailsEndpoint, language }) => ({
+        repositories.map(({ name, id, description, url: detailsEndpoint, language }) => ({
           description,
           detailsEndpoint,
           id,
           language,
           name,
         })),
+    };
+  });
+};
+
+export const getRepositoryDetails = (repositoryUrl) => {
+  return get(repositoryUrl).then(({ data: details, error, message }) => {
+    return {
+      error,
+      message,
+      details:
+        (details && {
+          name: details.name,
+          description: details.description,
+          htmlUrl: details.html_url,
+          createdAt: details.created_at,
+          stargazers: details.stargazers_count,
+          watchers: details.watchers_count,
+          forks: details.forks,
+          issues: details.open_issues,
+          subscribers: details.subscribers_count,
+        }) ||
+        null,
     };
   });
 };
