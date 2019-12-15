8a5cae6ec934d4d369b76ac4de53d7aa182cae2b
Ömer Burak KARATAŞ <omer.burak.karatas@outlook.com>
2019-12-15 14:41:04 +0300

    feat: create RepositoryCard component

---
+++ app/components/repository-card/RepositoryCard.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { View, TouchableOpacity, Text, StyleSheet } from 'react-native';
+
+type Props = {
+  name: string,
+  description: string,
+  language: string,
+  detailsEndpoint: string,
+  onClick: () => any,
+};
+
+export const RepositoryCard = ({ name, description, language, detailsEndpoint, onClick }: Props) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>{name}</Text>
+      <Text style={styles.description}>{description}</Text>
+      <View style={styles.footerContainer}>
+        <Text style={styles.language}>Language: {language}</Text>
+        <TouchableOpacity style={styles.buttonContainer} onPress={() => onClick(detailsEndpoint)}>
+          <Text style={styles.buttonText}>Details</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#ed5565',
+    marginBottom: 16,
+    padding: 16,
+    paddingBottom: 8,
+    borderRadius: 8,
+  },
+  title: {
+    color: '#ffcd69',
+    fontWeight: 'bold',
+    fontSize: 18,
+    marginBottom: 8,
+  },
+  description: { color: '#f5f7fa' },
+  footerContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 12,
+  },
+  language: { flex: 5, color: '#f5f7fa' },
+  buttonContainer: { borderRadius: 8, backgroundColor: '#4fc2e5' },
+  buttonText: {
+    color: 'white',
+    padding: 8,
+  },
+});
